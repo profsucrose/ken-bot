f4559caf713393afd00752c966b674ba84ef67f1
profsucrose <dr.blobbles@gmail.com>
2020-12-18 01:18:17 -0800

    Added reactions

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,40 @@
-  
+
+extern crate discord;
+extern crate dotenv;
+
+use discord::model::{EmojiId, Event, ReactionEmoji, UserId};
+use discord::Discord;
 use std::env;
-use dotenv::dotenv;
 
-use serenity::{async_trait, model::{channel::Message, gateway::Ready, id::UserId}, prelude::*};
+fn main() {
+    dotenv::dotenv().ok();
 
-struct Handler;
+	let discord = Discord::from_bot_token(&env::var("DISCORD_TOKEN").expect("Expected token"))
+		.expect("login failed");
 
-#[async_trait]
-impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.author.id == "734851161823248525" {
-
-        }
-
-
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-    }
-
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-    }
-}
-
-#[tokio::main]
-async fn main() {
-    dotenv().ok();
-    // Configure the client with your Discord bot token in the environment.
-    let token = env::var("DISCORD_TOKEN")
-        .expect("Expected a token in the environment");
-
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
-    let mut client = Client::builder(&token)
-        .event_handler(Handler)
-        .await
-        .expect("Err creating client");
-
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
-    if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
-    }
+	let (mut connection, _) = discord.connect().expect("connect failed");
+    
+    println!("Started Ken Bot");
+	loop {
+		match connection.recv_event() {
+			Ok(Event::MessageCreate(message)) => {
+                if message.author.id == UserId(734851161823248525) {
+                    discord.add_reaction(
+                        message.channel_id, 
+                        message.id, 
+                        ReactionEmoji::Custom { 
+                            name: String::from("downvote"), 
+                            id: EmojiId(773668231327121448) 
+                        }
+                    ).expect("Failed to react to message with :downvote:");    
+                }
+			}
+			Ok(_) => {}
+			Err(discord::Error::Closed(code, body)) => {
+				println!("Gateway closed with code {:?}: {}", code, body);
+				break;
+			}
+			Err(err) => println!("Receive error: {:?}", err),
+		}
+	}
 }
